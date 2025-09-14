1c0e52a150cb8d1b9c66206a906e0a6e05c190d3
mkhlz <24216278+mkhlz@users.noreply.github.com>
2025-09-14 12:33:36 +0530

    problem 872

--- 0872-Leaf-Similar-Trees/solution.cpp
+++ 0872-Leaf-Similar-Trees/solution.cpp
@@ -10,10 +10,22 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *l, TreeNode *r) : val(x), left(l), right(r) {}
 };
-
 class Solution {
 public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        
+        vector<int> leaves1, leaves2;
+        getLeaves(root1, leaves1);
+        getLeaves(root2, leaves2);
+        return leaves1 == leaves2;
+    }
+private:
+    // DFS
+    void getLeaves(TreeNode* node, vector<int>& leaves) {
+      if (!node) return;
+      if (!node->left && !node->right) {
+        leaves.push_back(node->val);
+      }
+      getLeaves(node->left, leaves);
+      getLeaves(node->right, leaves);
     }
 };

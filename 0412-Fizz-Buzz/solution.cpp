79b4e2d61c9e724f55fd39668e5e5c67ab971781
mkhlz <24216278+mkhlz@users.noreply.github.com>
2025-09-26 23:47:24 +0530

    problem 0412

---
+++ 0412-Fizz-Buzz/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define MAX_N 10001
+#define MOD 1000000007
+#define apo(a,b) sqrt( powl(a.x-b.x,2) + powl(a.y-b.y,2) )
+#define ldeq(a,b) ( fabsl(a-b) < 0.0000000007 )
+#define mmodeq(a,b) ((a>=0)?((a%b)%b):((((a+((abs(a)+b)/b)*b))%b)%b))
+#define rep(a,b) for(int i=a; i<b; ++i)
+#define repv(a,b,i) for(int i=a; i<b; ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define S(x) ((int)(x).size())
+#define PB push_back
+#define fi first
+#define se second
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> s;
+        rep(1, n+1){
+            if (i % 3 == 0 && i % 5 == 0) s.push_back("Fizz Buzz");
+            else if (i % 3 == 0) s.push_back("Fizz");
+            else if (i % 5 == 0) s.push_back("Buzz");
+            else s.push_back(to_string(i));
+        }
+    }
+};
